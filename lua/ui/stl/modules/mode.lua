4b476cad0c75e4ceb56f80781423d5b7c09cf954
AlexRosas <rosasrias82@gmail.com>
2024-06-03 18:36:03 -0500

    Second commit

--- lua/ui/stl/modules/mode.lua
+++ lua/ui/stl/modules/mode.lua
@@ -6,8 +6,9 @@ local M = function(m)
   local mode = api.nvim_get_mode().mode
   local sep = "%#" .. modes[mode][2] .. "Sep" .. "#" .. "  "
   local septwo = "%#StalineModeSepTwo" .. "#" .. " "
+  local septhree = "%#StalineModeSepThree" .. "#" .. "  "
   if (m == 'blocks') then
-    return "%#" .. modes[mode][2] .. "#" .. "  " .. modes[mode][1] .. " ";
+    return "%#" .. modes[mode][2] .. "#" .. " " .. modes[mode][1] .. " " .. septhree;
   elseif (m == 'minimal') then
     return "%#" .. "StalineModeMinimal" .. "#" .. " " .. modes[mode][1] .. " ";
   elseif (m == 'fancy') then
