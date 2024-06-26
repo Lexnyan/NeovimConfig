6e408fe2d56784d2a475d86cd861e0c9c20e0263
Alex Diego <rosasrias82@gmail.com>
2024-06-26 10:17:17 -0500

    Refactor code

--- lua/core/functions.lua
+++ lua/core/functions.lua
@@ -1,137 +1,109 @@
 local M = {}
 
-function M.toggle_option(option)
+local function notify(message, level, options)
+  vim.notify(message, level or vim.log.levels.INFO, options)
+end
+
+local function substitute(cmd)
+  return cmd:gsub("%%", vim.fn.expand "%")
+      :gsub("$fileBase", vim.fn.expand "%:r")
+      :gsub("$filePath", vim.fn.expand "%:p")
+      :gsub("$file", vim.fn.expand "%")
+      :gsub("$dir", vim.fn.expand "%:p:h")
+      :gsub("#", vim.fn.expand "#")
+      :gsub("$altFile", vim.fn.expand "#")
+end
+
+local function toggle_option(option)
   local value = not vim.api.nvim_get_option_value(option, {})
   vim.opt[option] = value
-  vim.notify(option .. " set to " .. tostring(value))
+  notify(option .. " set to " .. tostring(value))
 end
 
-function M.toggle_tabline()
+local function toggle_tabline()
   local value = vim.api.nvim_get_option_value("showtabline", {})
-  if value == 2 then
-    value = 0
-  else
-    value = 2
-  end
+  value = value == 2 and 0 or 2
   vim.opt.showtabline = value
-  vim.notify("showtabline" .. " set to " .. tostring(value))
+  notify("showtabline set to " .. tostring(value))
 end
 
-function M.build_run()
-  -- HUUUUUUUUUUUUUUUUUUUUUUUGE kudos and thanks to
-  -- https://github.com/hown3d for this function <3
-  local function substitute(cmd)
-    cmd = cmd:gsub("%%", vim.fn.expand "%")
-    cmd = cmd:gsub("$fileBase", vim.fn.expand "%:r")
-    cmd = cmd:gsub("$filePath", vim.fn.expand "%:p")
-    cmd = cmd:gsub("$file", vim.fn.expand "%")
-    cmd = cmd:gsub("$dir", vim.fn.expand "%:p:h")
-    cmd = cmd:gsub("#", vim.fn.expand "#")
-    cmd = cmd:gsub("$altFile", vim.fn.expand "#")
-
-    return cmd
-  end
-
+local function build_run()
   local file_extension = vim.fn.expand "%:e"
-  local selected_cmd = ""
   local term_cmd = "bot 10 new | term "
+
   local supported_filetypes = {
-    html = {
-      default = "live-server .",
-    },
-    c = {
-      default = "gcc % -o $fileBase && ./$fileBase",
-      debug = "gcc -g % -o $fileBase && $fileBase",
-    },
-    cs = {
-      default = "dotnet run",
-    },
-    cpp = {
-      default = "g++ % -o  $fileBase && ./$fileBase",
-      debug = "g++ -g % -o  $fileBase",
-      -- competitive = "g++ -std=c++17 -Wall -DAL -O2 % -o $fileBase && $fileBase<input.txt",
-      competitive = "g++ -std=c++17 -Wall -DAL -O2 % -o $fileBase && $fileBase",
-    },
-    py = {
-      default = "python %",
-    },
-    go = {
-      default = "go run %",
-    },
-    java = {
-      default = "java %",
-    },
-    js = {
-      default = "node %",
-      debug = "node --inspect %",
-    },
-    ts = {
-      default = "tsc % && node $fileBase",
-    },
-    rs = {
-      default = "rustc % && $fileBase",
-    },
-    php = {
-      default = "php %",
-    },
-    r = {
-      default = "Rscript %",
-    },
-    jl = {
-      default = "julia %",
-    },
-    rb = {
-      default = "ruby %",
-    },
-    pl = {
-      default = "perl %",
-    },
+    html = { default = "live-server ." },
+    c = { default = "gcc % -o $fileBase && ./$fileBase", debug = "gcc -g % -o $fileBase && $fileBase" },
+    cs = { default = "dotnet run" },
+    cpp = { default = "g++ % -o  $fileBase && ./$fileBase", debug = "g++ -g % -o  $fileBase", competitive = "g++ -std=c++17 -Wall -DAL -O2 % -o $fileBase && $fileBase" },
+    py = { default = "python %" },
+    go = { default = "go run %" },
+    java = { default = "java %" },
+    js = { default = "node %", debug = "node --inspect %" },
+    ts = { default = "tsc % && node $fileBase" },
+    rs = { default = "rustc % && $fileBase" },
+    php = { default = "php %" },
+    r = { default = "Rscript %" },
+    jl = { default = "julia %" },
+    rb = { default = "ruby %" },
+    pl = { default = "perl %" },
   }
 
-  if supported_filetypes[file_extension] then
-    local choices = vim.tbl_keys(supported_filetypes[file_extension])
-
-    if #choices == 0 then
-      vim.notify("It doesn't contain any command", vim.log.levels.WARN, { title = "Code Runner" })
-    elseif #choices == 1 then
-      selected_cmd = supported_filetypes[file_extension][choices[1]]
-      vim.cmd(term_cmd .. substitute(selected_cmd))
-    else
-      vim.ui.select(choices, { prompt = "Choose a command: " }, function(choice)
-        selected_cmd = supported_filetypes[file_extension][choice]
-        if selected_cmd then
-          vim.cmd(term_cmd .. substitute(selected_cmd))
-        end
-      end)
-    end
-  else
-    vim.notify("The filetype isn't included in the list", vim.log.levels.WARN, { title = "Code Runner" })
+  local function execute_command(cmd)
+    vim.cmd(term_cmd .. substitute(cmd))
   end
+
+  if not supported_filetypes[file_extension] then
+    notify("The filetype isn't included in the list", vim.log.levels.WARN, { title = "Code Runner" })
+    return
+  end
+
+  local choices = vim.tbl_keys(supported_filetypes[file_extension])
+  if #choices == 0 then
+    notify("It doesn't contain any command", vim.log.levels.WARN, { title = "Code Runner" })
+    return
+  end
+
+  if #choices == 1 then
+    execute_command(supported_filetypes[file_extension][choices[1]])
+    return
+  end
+
+  vim.ui.select(choices, { prompt = "Choose a command: " }, function(choice)
+    local selected_cmd = supported_filetypes[file_extension][choice]
+    if selected_cmd then
+      execute_command(selected_cmd)
+    end
+  end)
 end
 
-local lazygit = require("toggleterm.terminal").Terminal:new({
-  cmd = "lazygit",
-  dir = "git_dir",
-  direction = "float",
-  float_opts = { border = "curved" },
-  on_open = function(term)
-    vim.cmd("startinsert!")
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-  end,
-})
-
-function M.LazyGit()
+local function lazygit_toggle()
+  local lazygit = require("toggleterm.terminal").Terminal:new({
+    cmd = "lazygit",
+    dir = "git_dir",
+    direction = "float",
+    float_opts = { border = "curved" },
+    on_open = function(term)
+      vim.cmd("startinsert!")
+      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+    end,
+  })
   lazygit:toggle()
 end
 
-function M.Ranger()
-  local status_ok, _ = pcall(require, "toggleterm")
+local function ranger_toggle()
+  local status_ok, toggleterm = pcall(require, "toggleterm")
+
   if not status_ok then
-    return vim.notify("toggleterm.nvim isn't installed!")
+    notify("toggleterm.nvim isn't installed!")
+    return
   end
+
   if vim.fn.executable("ranger") == 0 then
-    return vim.notify("ranger isn't installed")
+    notify("ranger isn't installed")
+    return
   end
+
   local ranger = require("toggleterm.terminal").Terminal:new({
     cmd = "ranger",
     direction = "float",
@@ -141,5 +113,11 @@ function M.Ranger()
   })
   ranger:toggle()
 end
+
+M.toggle_option = toggle_option
+M.toggle_tabline = toggle_tabline
+M.build_run = build_run
+M.lazygit_toggle = lazygit_toggle
+M.ranger_toggle = ranger_toggle
 
 return M
