49be183823cff13cd9201be566a022cb9e3c08d1
Alex Diego <rosasrias82@gmail.com>
2024-06-26 15:08:45 -0500

    Refactory bufferline code

--- lua/ui/buf/fn.lua
+++ lua/ui/buf/fn.lua
@@ -1,16 +1,26 @@
 local M = {}
-M.bufilter = function()
-  local bufs = vim.api.nvim_list_bufs() or nil
 
-  if not bufs then
+-- Utility function to get the buffer name or an empty string if the buffer is not named
+local function get_buffer_name(buf)
+  return (#vim.api.nvim_buf_get_name(buf) ~= 0) and vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":t") or ""
+end
+
+-- Utility function to check if a buffer is valid and listed
+local function is_buffer_valid_and_listed(buf)
+  return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted and vim.api.nvim_buf_is_loaded(buf)
+end
+
+-- Function to filter buffers according to specific criteria
+M.bufilter = function()
+  local bufs = vim.api.nvim_list_bufs() or {}
+
+  -- Guard clause for empty buffer list
+  if #bufs == 0 then
     return {}
   end
 
   for i = #bufs, 1, -1 do
-    local filename = (#vim.api.nvim_buf_get_name(bufs[i]) ~= 0) and
-        vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufs[i]), ":t") or
-        ""
-    if not vim.api.nvim_buf_is_valid(bufs[i]) or not vim.bo[bufs[i]].buflisted or not vim.api.nvim_buf_is_loaded(bufs[i]) or filename == '' then
+    if not is_buffer_valid_and_listed(bufs[i]) or get_buffer_name(bufs[i]) == '' then
       table.remove(bufs, i)
     end
   end
@@ -18,37 +28,46 @@ M.bufilter = function()
   return bufs
 end
 
+-- Function to switch to the previous buffer
 M.tabuflinePrev = function()
   local bufs = M.bufilter()
-  for i, v in ipairs(bufs) do
-    if vim.api.nvim_get_current_buf() == v then
+  local current_buf = vim.api.nvim_get_current_buf()
+
+  for i, buf in ipairs(bufs) do
+    if current_buf == buf then
       vim.cmd(i == 1 and "b" .. bufs[#bufs] or "b" .. bufs[i - 1])
       break
     end
   end
 end
+
+-- Function to switch to the next buffer
 M.tabuflineNext = function()
   local bufs = M.bufilter()
+  local current_buf = vim.api.nvim_get_current_buf()
 
-  for i, v in ipairs(bufs) do
-    if vim.api.nvim_get_current_buf() == v then
+  for i, buf in ipairs(bufs) do
+    if current_buf == buf then
       vim.cmd(i == #bufs and "b" .. bufs[1] or "b" .. bufs[i + 1])
       break
     end
   end
 end
 
-
+-- Function to close a buffer
 M.close_buffer = function(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+  -- Guard clause for terminal buffer
   if vim.bo.buftype == "terminal" then
     vim.cmd(vim.bo.buflisted and "set nobl | enew" or "hide")
-  else
-    bufnr = bufnr or vim.api.nvim_get_current_buf()
-    if bufnr == vim.api.nvim_get_current_buf() then
-      require("ui.buf.fn").tabuflinePrev()
-    end
-    vim.cmd("confirm bd" .. bufnr)
+    return
   end
+
+  if bufnr == vim.api.nvim_get_current_buf() then
+    M.tabuflinePrev()
+  end
+  vim.cmd("confirm bd" .. bufnr)
 end
 
 return M
--- lua/ui/buf/init.lua
+++ lua/ui/buf/init.lua
@@ -1,86 +1,54 @@
 local M = {}
 
--- Neovim commands
-vim.cmd "function! BufflineGoToBuf(bufnr,b,c,d) \n execute 'b'..a:bufnr \n endfunction"
-vim.cmd [[
-   function! BufflineKillBuf(bufnr,b,c,d)
-        call luaeval('require("ui.buf.fn").close_buffer(_A)', a:bufnr)
-  endfunction]]
-vim.cmd "function! ToggleTheme(a,b,c,d) \n lua require('theme.pick'):random() \n endfunction"
-vim.cmd "function! ToggleTrans(a,b,c,d) \n NvimToggleTrans \n endfunction"
-vim.cmd "function! CloseAll(a,b,c,d) \n q \n endfunction"
-vim.cmd "function! Split(a,b,c,d) \n vsplit \n endfunction"
-vim.cmd "function! Run(a,b,c,d) \n lua require('core.functions').build_run() \n endfunction"
-
-vim.api.nvim_create_user_command("BufflinePrev", function()
-  require("ui.buf.fn").tabuflinePrev()
-end, {})
-vim.api.nvim_create_user_command("BufflineNext", function()
-  require("ui.buf.fn").tabuflineNext()
-end, {})
-
--- Function to create a tab
-local createTab = function(buf)
-  local close_btn = "%" .. buf .. "@BufflineKillBuf@ 󰅜 %X"
-  local filename = (#vim.api.nvim_buf_get_name(buf) ~= 0) and vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":t") or
-      ""
-
-  -- Logic to determine if the filename is the same as the current buffer
-  for _, buffer in pairs(vim.api.nvim_list_bufs()) do
-    if vim.api.nvim_buf_is_valid(buffer) and vim.api.nvim_buf_is_loaded(buffer) and vim.bo[buffer].buflisted and filename ~= "" then
-      if filename == vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buffer), ":t") and buffer ~= buf then
-        local other = {}
-        for match in (vim.api.nvim_buf_get_name(buffer) .. "/"):gmatch("(.-)" .. "/") do
-          table.insert(other, match)
-        end
-
-        local current = {}
-        for match in (vim.api.nvim_buf_get_name(buf) .. "/"):gmatch("(.-)" .. "/") do
-          table.insert(current, match)
-        end
-
-        filename = current[#current]
-
-        for i = #current - 1, 1, -1 do
-          local value_current = current[i]
-          local other_current = other[i]
-
-          if value_current ~= other_current then
-            filename = value_current .. '/' .. filename
-            break
-          end
-        end
-        break
-      end
-    end
-  end
-
-  -- Settigns colors and buttons based on buffer status
-  if buf == vim.api.nvim_get_current_buf() then
-    filename = "%#BufflineBufOnActive#  " .. "  " .. filename
-    close_btn = (vim.bo[0].modified and "%" .. buf .. "@BufflineKillBuf@%#BuffLineBufOnModified#   ")
-        or ("%#BuffLineBufOnClose#" .. close_btn) .. " "
-  else
-    filename = "%#BufflineBufOnInactive#  " .. "  " .. filename
-    close_btn = (vim.bo[buf].modified and "%" .. buf .. "@BufflineKillBuf@%#BuffLineBufOffModified#   ")
-        or ("%#BuffLineBufOffClose#" .. close_btn) .. " "
-  end
-  return "%" .. buf .. "@BufflineGoToBuf@" .. filename .. "  " .. close_btn .. '%X' .. "%#BufflineEmptyColor#"
+-- Utility function to check if a buffer is valid and listed
+local function is_buffer_valid_and_listed(buf)
+  return vim.api.nvim_buf_is_valid(buf)
+      and vim.bo[buf].buflisted
+      and vim.api.nvim_buf_is_loaded(buf)
 end
 
-local excludedFileTypes = { 'NvimTree', 'help', 'dasher', 'lir', 'alpha', "toggleterm" }
+-- Utility function to get the buffer name or an empty string if the buffer is not named
+local function get_buffer_name(buf)
+  local buf_name = vim.api.nvim_buf_get_name(buf)
+  return (#buf_name ~= 0) and vim.fn.fnamemodify(buf_name, ":t") or ""
+end
 
-local treeWidth = function()
+-- Function to create a tab string for a buffer
+local function create_tab_string(buf)
+  if not is_buffer_valid_and_listed(buf) then
+    return ""
+  end
+
+  local buf_name = get_buffer_name(buf)
+  local is_active = (buf == vim.api.nvim_get_current_buf())
+  local is_modified = vim.bo[buf].modified
+  local buf_highlight = is_active and "%#BufflineBufOnActive#" or "%#BufflineBufOnInactive#"
+  local close_btn_highlight = is_active and "%#BuffLineBufOnClose#" or "%#BuffLineBufOffClose#"
+
+  if is_modified then
+    close_btn_highlight = is_active and "%#BuffLineBufOnModified#" or "%#BuffLineBufOffModified#"
+  end
+
+  local close_btn = "%" .. buf .. "@BufflineKillBuf@ 󰅜 %X"
+  close_btn = close_btn_highlight .. close_btn .. " "
+
+  return buf_highlight .. "  " .. buf_name .. close_btn .. "%#BufflineEmptyColor#"
+end
+
+-- Function to get the width of the NvimTree window
+local function get_tree_width()
   for _, win in pairs(vim.api.nvim_tabpage_list_wins(0)) do
-    if vim.bo[vim.api.nvim_win_get_buf(win)].ft == "NvimTree" then
+    local buf = vim.api.nvim_win_get_buf(win)
+    if vim.bo[buf].ft == "NvimTree" then
       return vim.api.nvim_win_get_width(win)
     end
   end
   return 0
 end
 
-M.getTabline = function()
-  local buffline = " "
+-- Function to generate the entire tabline string
+M.get_tabline = function()
+  local buffline = ""
   local buffstart = "%#BuffLineEmpty#"
   local run = "%#BuffLineRun# %@Run@" .. "  "
   if vim.bo.filetype == "html" then
@@ -91,39 +59,47 @@ M.getTabline = function()
   local split = "%#BuffLineSplit# %@Split@" .. "  "
   local closebutton = "%#BufflineCloseButton# %@CloseAll@" .. "󰅜 "
   local counter = 0
+
   for _, buf in pairs(vim.api.nvim_list_bufs()) do
-    local filename = vim.api.nvim_buf_get_name(buf)
-    if filename ~= "" and not vim.tbl_contains(excludedFileTypes, vim.bo[buf].ft) then
-      if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted then
-        filename = "%#BufflineEmptyColor#" .. createTab(buf)
-        buffline = buffline .. filename
+    if is_buffer_valid_and_listed(buf) then
+      local filename = vim.api.nvim_buf_get_name(buf)
+      local buf_type = vim.bo[buf].ft
+      if filename ~= "" and not vim.tbl_contains({ 'NvimTree', 'help', 'dasher', 'lir', 'alpha', "toggleterm" }, buf_type) then
+        local tab_string = create_tab_string(buf)
+        buffline = buffline .. tab_string
         counter = counter + 1
       end
     end
   end
+
   if counter > 0 then
     buffstart = "%#BufflineEmptyColor#"
   end
-  local treespace
-  if treeWidth() > 2 then
-    treespace = "%#BufflineTree#" ..
-        string.rep(" ", treeWidth() / 2 - 3) .. "Browse" .. string.rep(" ", treeWidth() / 2 - 2)
+
+  local treespace = ""
+  local tree_width = get_tree_width()
+
+  if tree_width > 2 then
+    treespace = ("%#BufflineTree#" .. string.rep(" ", tree_width / 2 - 3) .. "Browse" .. string.rep(" ", tree_width / 2 - 2))
   else
-    treespace = "%#BufflineTree#" .. string.rep(" ", treeWidth())
+    treespace = "%#BufflineTree#" .. string.rep(" ", tree_width)
   end
+
   return treespace .. buffstart .. buffline .. "%=" .. run .. split .. trans .. button .. closebutton
 end
 
+-- Setup function
 M.setup = function()
   if #vim.fn.getbufinfo { buflisted = 1 } >= 1 then
     vim.opt.showtabline = 2
-    vim.opt.tabline = '%!v:lua.require("ui.buf").getTabline()'
+    vim.opt.tabline = '%!v:lua.require("ui.buf").get_tabline()'
   end
 end
 
+-- Mappings
 vim.cmd [[
-nnoremap <silent><TAB> :BufflineNext<CR>
-nnoremap <silent><S-TAB> :BufflinePrev<CR>
+  nnoremap <silent><TAB> :BufflineNext<CR>
+  nnoremap <silent><S-TAB> :BufflinePrev<CR>
 ]]
 
 return M
